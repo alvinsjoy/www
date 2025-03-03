42a489e321837fee73cdf0d72d43039738c64062
Arjunpanicker2005 <127143850+Arjunpanicker2005@users.noreply.github.com>
2025-03-03 19:03:48 +0530

    updated index.py

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
-from api import signalyse
+from fastapi.middleware.cors import CORSMiddleware  
+from api import signalyse  
 
-app=FastAPI()
+app = FastAPI()
+
+
 app.include_router(signalyse.router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://signalyze.vercel.app/"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
